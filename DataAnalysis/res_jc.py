209d093ead7b077f9b56805ccb0e07d98bbe3c9e
AppSaintre <cosecode@gmail.com>
2017-03-20 16:37:43 +0900

    Update res_jc.py

--- DataAnalysis/res_jc.py
+++ DataAnalysis/res_jc.py
@@ -1,7 +1,9 @@
+from os import listdir
+from os.path import isfile, join
 import re
 
 marks={
-		'init_memory_allocate':0,
+	'init_memory_allocate':0,
         'init_memory_initialize':0,
         'init_memory_download':0,
         'cpu_readback':0,
@@ -29,31 +31,35 @@ marks={
         'gpu_overlap':0
 }
 
+def dataBD(fpath):
+	fl = open(fpath)
+	for line in fl:
+		if 'Result: init' in line or 'Result: cpu' in line or 'Result: gpu' in line or 'Result: free' in line:
+			mk = re.findall(': ([a-z,_]+) [0-9]+',line)
+			val = re.findall(' ([0-9,.]+)',line)
+			#print(mk[0],':',val[0])
+			marks[mk[0]] += float(val[0])
+	#print(marks)
+	init = marks['init_memory_allocate']+marks['init_memory_initialize']+marks['init_memory_download']
+	cpucalc = marks['cpu_branch']+marks['cpu_bound']+marks['cpu_maximum']+marks['cpu_label']+marks['cpu_scan']+marks['cpu_packing']
+	g1 = marks['gpu_overlap']
+	g2= marks['gpu_maximum']+marks['gpu_label']+marks['gpu_scan']+marks['gpu_packing']
+	#gpucalc = marks['gpu_bandb']
+	singledw = marks['gpu_single_memcpy']
+	cpumrb = marks['cpu_deva_readback'] + marks['cpu_devb_readback']
+	ft = marks['free_memory']
+	print('Init Time, Preparation of Memories:',init)
+	print('CPU Calculation Time:',cpucalc)
+	#print('GPU Calculation Time:',gpucalc)
+	print('GPU Calculation Time detail1:',g1)
+	print('GPU Calculation Time detail2:',g2)
+	print('GPU-singlemode Download Time:',singledw)
+	print('CPU-mode Readback Time:',cpumrb)
+	print('Free Memory Time:',ft)
+	print('Total:',init+cpucalc+g1+g2+singledw+cpumrb+ft)
+	
+mypath = 'script_data/' 
+files = [f for f in listdir(mypath) if isfile(join(mypath, f))]
 
-fpath = 'script_data/9141.txt' 
-fl = open(fpath)
-
-for line in fl:
-	if 'Result: init' in line or 'Result: cpu' in line or 'Result: gpu' in line or 'Result: free' in line:
-		mk = re.findall(': ([a-z,_]+) [0-9]+',line)
-		val = re.findall(' ([0-9,.]+)',line)
-		#print(mk[0],':',val[0])
-		marks[mk[0]] += float(val[0])
-#print(marks)
-init = marks['init_memory_allocate']+marks['init_memory_initialize']+marks['init_memory_download']
-cpucalc = marks['cpu_branch']+marks['cpu_bound']+marks['cpu_maximum']+marks['cpu_label']+marks['cpu_scan']+marks['cpu_packing']
-g1 = marks['gpu_overlap']
-g2= marks['gpu_maximum']+marks['gpu_label']+marks['gpu_scan']+marks['gpu_packing']
-#gpucalc = marks['gpu_bandb']
-singledw = marks['gpu_single_memcpy']
-cpumrb = marks['cpu_deva_readback'] + marks['cpu_devb_readback']
-ft = marks['free_memory']
-print('Init Time, Preparation of Memories:',init)
-print('CPU Calculation Time:',cpucalc)
-#print('GPU Calculation Time:',gpucalc)
-print('GPU Calculation Time detail1:',g1/2)
-print('GPU Calculation Time detail2:',g2/2)
-print('GPU-singlemode Download Time:',singledw)
-print('CPU-mode Readback Time:',cpumrb)
-print('Free Memory Time:',ft)
-print('Total:',init+cpucalc+g1/2+g2/2+singledw+cpumrb+ft)
+for f in files:
+	dataBD(f)
